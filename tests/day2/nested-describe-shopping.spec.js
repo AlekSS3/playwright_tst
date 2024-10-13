14091fd2553b91d79900241d16fd3a8cbffa0060
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-13 22:34:53 +0200

    Added nested-describe-shopping.spec.js and nested-describe-sorting.spec.js

---
+++ tests/day2/nested-describe-shopping.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+test.describe("Test Suite for Shopping Cart Tests", () => {
+
+    test.describe("Test Suite for addding products to cart", () => {
+
+        test('adding one product', async ({ page }) => {
+        
+        });
+
+        test('adding multiple products', async ({ page }) => {
+        
+        });
+    });
+
+    test.describe("Test Suite for removing products from cart", () => {
+
+        test('removing one product', async ({ page }) => {
+        
+        });
+
+    });
+
+});

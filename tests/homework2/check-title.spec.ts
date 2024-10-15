616c765932014404eca1778180947e74b69e1f47
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-15 18:05:46 +0200

    added new test from homework2 - check-title.spec.ts

---
+++ tests/homework2/check-title.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Check title of a website', async ({ page }) => {
+    //Navigate to the website
+    await page.goto('https://saucedemo.com');
+
+    const title =await page.title();
+
+    //Assertions
+    await expect(page).toHaveTitle(/Swag Labs/);
+    expect (title).toBe('Swag Labs');
+    
+  });

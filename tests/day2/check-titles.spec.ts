2495eb42e71ed2ebd8d6f65c1323b68bb19ef94d
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-15 23:58:24 +0200

    refactoring

--- tests/day2/check-titles.spec.ts
+++ tests/day2/check-titles.spec.ts
@@ -1,30 +1,28 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test.describe('Test Suites for e-commerce website titles', () => {
+// test.describe('Test Suites for e-commerce website titles', () => {
 
-    test('Check title of the login page', async ({ page }) => {
-        await page.goto('https://saucedemo.com');
+//     test('Check title of the login page', async ({ page }) => {
+//         await page.goto('https://saucedemo.com');
         
-        const title =await page.title();
+//         const title =await page.title();
 
-        // Assertion
-        await expect(page).toHaveTitle('Swag Labs');
-        expect (title).toBe('Swag Labs');
+//         // Assertion
+//         await expect(page).toHaveTitle('Swag Labs');
+//         expect (title).toBe('Swag Labs');
 
-    });
+//     });
 
-    test('Check title of the products page', async ({ page }) => {
-        await page.goto('https://saucedemo.com');
-        await page.locator('[data-test="username"]').fill('standard_user');
-        await page.locator('[data-test="password"]').fill('secret_sauce');
-        await page.locator('[data-test="login-button"]').click();
+//     test('Check title of the products page', async ({ page }) => {
+//         await page.goto('https://saucedemo.com');
+//         await page.locator('[data-test="username"]').fill('standard_user');
+//         await page.locator('[data-test="password"]').fill('secret_sauce');
+//         await page.locator('[data-test="login-button"]').click();
 
-        const title1 =await page.title();
-
-        // Assertion
-        expect (title1).toBe('Products');
+//         // Assertion
+//         expect(title).toBe('Products');
     
 
-    });
+//     });
 
-});
+// });

1f924d49f7bb9532b0b5a9bb422c61caaae58f2b
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-24 21:31:03 +0200

    added tests to 'dynamic-properties'

---
+++ tests/homework#3/Elements/dynamic-properties.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Test suite for dynamic properties', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByText('Elements').click();
+        await page.getByText('Dynamic Properties').click();
+
+    }); 
+
+        test('Test for enabling button afer 5 seconds delay', async ({ page }) => {
+            //const button = page.getByAltText('Will enable 5 seconds');
+            await page.getByRole('button', { name: 'Will enable 5 seconds' }).click();
+            await page.getByRole('button', { name: 'Visible After 5 Seconds' }).click();
+    });
+
+        test('', async ({ page }) => {
+            await page.getByRole('button', { name: 'Color Change' }).click();
+            
+    });
+
+
+
+});

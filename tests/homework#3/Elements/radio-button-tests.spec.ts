82019b8c9f332fdd2079ae0762b3fdaf92530cfe
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-24 12:19:14 +0200

    added tests for 'radio-button-test.spec.ts

---
+++ tests/homework#3/Elements/radio-button-tests.spec.ts
@@ -0,0 +1,36 @@
+import {test, expect} from '@playwright/test'
+
+test.describe('Test suite for radio button actions', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByText('Elements').click();
+        await page.getByText('Radio Button').click();
+
+        }); 
+
+    test('@fast est for clicking radio button "Yes"', async ({ page }) => {
+        await page.getByText('Yes').click();
+
+    });
+
+    test('@fast test for clicking radio button "Impressive"', async ({ page }) => {
+        await page.getByText('Impressive').click();
+
+    });
+
+    test('Test for checking unclickable disabled radio button "No"', async ({ page }) => {
+        const noRadioButton = page.locator('input[id="noRadio"]');
+        await expect (noRadioButton).toBeDisabled();
+        
+    });
+
+    test('Test for multiple clicks on radio buttons', async ({ page }) => {
+        await page.getByText('Yes').click();
+        await page.getByText('Impressive').click();
+        await page.getByText('Yes').click();
+        await page.getByText('Impressive').click();
+    });
+
+
+});

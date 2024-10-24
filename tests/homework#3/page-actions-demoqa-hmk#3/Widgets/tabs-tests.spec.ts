bc49e922561361bd5f891b8dfab25326b2bd1ca4
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-25 01:59:07 +0200

    added tests to a file 'tabs-tests.spec.ts'

--- tests/homework#3/page-actions-demoqa-hmk#3/Widgets/tabs-tests.spec.ts
+++ tests/homework#3/page-actions-demoqa-hmk#3/Widgets/tabs-tests.spec.ts
@@ -1,26 +1,55 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Test suite for accordian', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://demoqa.com');
-    await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
-    
+test.describe('Test suite for tabs', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByRole('heading', { name: 'Widgets' }).click();
+        await page.getByText('Tabs').click();
 
 });
 
-test('', async ({ page }) => {
-    
-    //Assertion
-  
+    test('Test for verifying first tab on the webapage', async ({ page }) => {
+     
+        //Assertion
+        await expect(page.getByText('Lorem Ipsum is simply dummy')).toBeVisible();
+
+    });
+
+    test('Test for verifying second tab on the webapage', async ({ page }) => {
+        await page.getByRole('tab', { name: 'Origin' }).click();
+        await page.getByText('Contrary to popular belief,').click();
+        await page.getByText('The standard chunk of Lorem').click();
+        
+        //Assertions
+        await expect(page.getByText('Contrary to popular belief,')).toBeVisible();
+        await expect(page.getByText('The standard chunk of Lorem')).toBeVisible();
+    });
+
+    test('Test for verifying third tab on the webapage', async ({ page }) => {
+        await page.getByRole('tab', { name: 'Use' }).click();
+
+        //Assertion
+
+        await expect(page.getByText('It is a long established fact')).toBeVisible();
+
+    });
+
+    test('Test for verifying fourth tab on the webapage', async ({ page }) => {
+        await page.getByText('WhatOriginUseMore').click();
+
+    });
+
+    test('Test for clicking on multiple tabs on the webpage', async ({ page }) => {
+        await page.getByRole('tab', { name: 'Origin' }).click();
+        await page.getByRole('tab', { name: 'Use' }).click();
+        await page.getByRole('tab', { name: 'What' }).click();
+        await page.getByText('WhatOriginUseMore').click();
+
+    });
+
+
+
 });
 
-test('T', async ({ page }) => {
-    
-    
-    //Assertion
-    
-});
 
 
-
-});

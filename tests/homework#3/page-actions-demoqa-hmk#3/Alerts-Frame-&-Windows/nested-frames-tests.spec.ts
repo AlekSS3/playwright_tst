0e1d6c6a21626f4ebb2f95880ad5b74cc48c4bf2
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-25 00:31:57 +0200

    added test in the file 'nested-frames-tests.spec.ts'

---
+++ tests/homework#3/page-actions-demoqa-hmk#3/Alerts-Frame-&-Windows/nested-frames-tests.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Test suite for nested frames', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://demoqa.com');
+    await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
+    await page.getByText('Nested Frames').click();
+
+
+});
+
+    test('Testing content inside the parent and nested frames on the webpage', async ({ page }) => {
+        const parentFrame = page.frameLocator('#frame1');
+        const childFrame = parentFrame.frameLocator('iframe');
+    
+        // Assertions:
+        await expect(childFrame.getByText('Child Iframe')).toBeVisible();
+        await expect(parentFrame.getByText('Parent frame')).toBeVisible();
+    });
+       
+
+});

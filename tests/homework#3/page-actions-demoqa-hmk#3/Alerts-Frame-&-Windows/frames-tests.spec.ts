16232fa7223d7c3637825e2409a823fb038140a5
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-25 00:07:27 +0200

    added test to file 'frames-tests.spec.ts'

---
+++ tests/homework#3/page-actions-demoqa-hmk#3/Alerts-Frame-&-Windows/frames-tests.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Test suite for frames', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://demoqa.com');
+    await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
+    await page.getByText('Frames', { exact: true }).click();
+
+});
+
+    test('Testing presence of two framse on the webpage', async ({ page }) => {
+        await expect(page.frameLocator('#frame1')).toBeDefined();
+        await expect(page.frameLocator('#frame2')).toBeDefined();
+    });
+
+    test('Testing content inside the two frames on the webpage', async ({ page }) => {
+        await page.locator('#frame1').contentFrame().locator('html').click();
+        await page.locator('#frame2').contentFrame().getByRole('heading', { name: 'This is a sample page' }).click();
+
+        //Assertion:
+        const frame1 = page.frameLocator('#frame1');
+        const frame2 = page.frameLocator('#frame2');
+        await expect(frame1.locator('h1')).toHaveText('This is a sample page');
+        await expect(frame2.locator('h1')).toHaveText('This is a sample page');
+    });
+       
+
+});

b3d88381a2d35bd70b2f2db210927d3bd0a1102b
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-25 14:16:58 +0200

    added test to a file 'select-menu-tests.spec.ts'

--- tests/homework#3/page-actions-demoqa-hmk#3/Widgets/select-menu-tests.spec.ts
+++ tests/homework#3/page-actions-demoqa-hmk#3/Widgets/select-menu-tests.spec.ts
@@ -1,26 +1,50 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Test suite for accordian', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://demoqa.com');
-    await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
+test.describe('Test suite for selecting menu', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByRole('heading', { name: 'Widgets' }).click();
+        await page.getByText('Select Menu').click();
     
+    });
 
-});
+        test('Test for selecting value', async ({ page }) => {
+            await page.locator('#withOptGroup svg').click();
+            await page.getByText('Group 1, option 1', { exact: true }).click();
+            
+            //Assertion
+            //await expect(page.getByText('Group 1, option 1')).toBeVisible();
+            await expect(page.locator('#withOptGroup .css-1uccc91-singleValue')).toHaveText('Group 1, option 1');
+    });
 
-test('', async ({ page }) => {
-    
-    //Assertion
-  
-});
+        test('Test for selecting "One"', async ({ page }) => {
+            await page.locator('#selectOne svg').click();
 
-test('T', async ({ page }) => {
-    
-    
-    //Assertion
-    
-});
+            //Assertion
+            await expect(page.getByText('Ms.', { exact: true })).toBeVisible();
 
+    });
 
+        test('Test for old style select menu', async ({ page }) => {
+            await page.locator('#oldSelectMenu').selectOption('4');
+            await page.locator('#oldSelectMenu').selectOption('3');
 
-});
+    });
+
+        test('Test for multiselect drop down', async ({ page }) => {
+            await page.locator('#selectMenuContainer svg').nth(2).click();
+            await page.locator('#react-select-10-option-1').click();
+            await page.locator('#react-select-10-option-3').click();
+        
+    });
+
+        test('Test for standard multiselect', async ({ page }) => {
+            await page.locator('#cars').selectOption('volvo');
+            await page.locator('#cars').selectOption('saab');
+            await page.locator('#cars').selectOption('opel');
+            await page.locator('#cars').selectOption('audi');
+          });
+        
+        
+        
+    });

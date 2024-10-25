cfd1bef1e60424864935de868c64917b71d12a18
YourCorrectUsername <stefanovska.a@gmail.com>
2024-10-25 12:16:36 +0200

    added tests to a file 'tool-tips-tests.spec.ts'

--- tests/homework#3/page-actions-demoqa-hmk#3/Widgets/tool-tips-tests.spec.ts
+++ tests/homework#3/page-actions-demoqa-hmk#3/Widgets/tool-tips-tests.spec.ts
@@ -1,26 +1,46 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Test suite for accordian', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://demoqa.com');
-    await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
+test.describe('Test suite for tool tips', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByRole('heading', { name: 'Widgets' }).click();
+        await page.getByText('Tool Tips').click();
+
+    });
+
+    test('Test button "Hover me to see"', async ({ page }) => {
+        await page.getByRole('button', { name: 'Hover me to see' }).hover();
+    
+        //Assertion
+        await expect(page.getByText('You hovered over the Button')).toBeVisible();
+
+    });
+
+    test('Test hover over text field', async ({ page }) => {
+        await page.getByPlaceholder('Hover me to see').hover();
+        
+        //Assertion
+        await expect(page.getByText('You hovered over the text')).toBeVisible();
+        
+    });
+
+
+    test('Test hover over a text link', async ({ page }) => {
+        await page.getByRole('link', { name: 'Contrary' }).hover();
+
+        //Assertion
+        await expect(page.getByText('You hovered over the Contrary')).toBeVisible();
+        
+    });
     
 
-});
+    test('Test hover over a link', async ({ page }) => {
+        await page.getByRole('link', { name: '1.10.32' }).hover();
 
-test('', async ({ page }) => {
-    
-    //Assertion
-  
-});
+        //Assertions
+        await expect(page.getByText('You hovered over the')).toBeVisible();
 
-test('T', async ({ page }) => {
-    
-    
-    //Assertion
-    
-});
-
-
-
-});
+        });
+   
+   
+    });

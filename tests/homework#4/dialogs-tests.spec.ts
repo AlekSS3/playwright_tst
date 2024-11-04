6efe322ff662bd000e7a7bc78194f96a3afec54b
YourCorrectUsername <stefanovska.a@gmail.com>
2024-11-04 23:11:17 +0100

    added dialog tests

---
+++ tests/homework#4/dialogs-tests.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Tests for dialogs', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com');
+        await page.getByRole('heading', { name: 'Alerts, Frame & Windows' }).click();
+        await page.getByText('Alerts', { exact: true }).click();
+    });
+
+    test('Test to accept dialog', async ({ page }) => {
+
+        page.on('dialog', async dialog => {
+            await dialog.accept();
+        });
+
+            await page.click('#confirmButton');
+
+            //Assertion
+            await expect(page.locator('#confirmResult')).toHaveText('You selected Ok');
+
+    });
+
+
+    test('Test to dismiss dialog', async ({ page }) => {
+
+        page.on('dialog', async dialog => {
+            await dialog.dismiss();
+        });
+
+            await page.click('#confirmButton');
+
+            //Assertion
+            await expect(page.locator('#confirmResult')).toHaveText('You selected Cancel');
+
+    });
+    
+
+
+})
